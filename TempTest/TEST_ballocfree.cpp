2bde028d1e605109581de8d9e0d28d6ab07eea0d
bing <xianyumo595250@gmail.com>
2019-06-20 23:43:38 +0800

    balloc()和bfree()

--- TempTest/TEST_ballocfree.cpp
+++ TempTest/TEST_ballocfree.cpp
@@ -2,18 +2,17 @@
 #include <iostream>
 
 using namespace std;
-//struct super_block super_block;
+struct super_block super_block;
+int datas = 0;
+int blocksi = 1;
 
 void format_sb_freedi() {
 
     //总块数可以先忽略根目录,etc,pwd分配的物理磁盘块,最后再分配
     // int free_dblock = ALLBLOCKNUM - (2 + dinodeBLK);
-    int free_dblock = 101;
-    //一共这么多个空闲数据块
+    int free_dblock = 201;
     super_block.s_nfree = free_dblock;
 
-    //初始化空闲块
-    //从最后一块开始分组
     for (int i = free_dblock / NICFREE; i > 0; i--) {
         if (i == free_dblock / NICFREE) {
             // 最后一块
@@ -21,24 +20,19 @@ void format_sb_freedi() {
             super_block.s_free[1] = 0;
         } else {
             super_block.s_free[0] = NICFREE;
-            super_block.s_free[1] =
-                0 +
-                (i * NICFREE - 1) * 1; //?改了 //指向下一个记录空闲块的物理块
+            super_block.s_free[1] = datas + (i * NICFREE - 1) * blocksi;
         }
         for (int j = 2; j < NICFREE + 1; j++) {
-            super_block.s_free[j] = 0 + (i * NICFREE - (j)) * 1; //?改了
+            super_block.s_free[j] = datas + (i * NICFREE - j) * blocksi;
         }
         if (i == 1) {
-            //第一个磁盘块
-            //写到第4个block
             super_block.s_free[0] = NICFREE - 3;
-            super_block.s_nfree -= 3; // 分配了三个,减3
+            super_block.s_nfree -= 3;
+            //写到 datas + 3 *　NICFREE * blocksi
             // TODO 把第一个空闲组 -> 写到第4个盘
-            // TODO 写回虚拟盘
 
         } else {
-            //写到 0 + (i-1) * NICFREE * 1
-            int k = 0 + ((i - 1) * NICFREE - 1) * 1; //?改了
+            //写到 datas + ((i-1) * NICFREE-1) * blocksi
             // TODO 写回虚拟盘
         }
         cout << i << " : ";
@@ -47,26 +41,84 @@ void format_sb_freedi() {
         cout << super_block.s_free[2] << ".." << super_block.s_free[NICFREE];
         cout << endl;
     }
-    // TODO 多出来的多余的记录到哪里?
+    // TODO 处理多出来的磁盘
 
-    //因为前三个已经被占用,所以空闲块堆栈指针指向当前空的第一个数据块->即第4个 
-    super_block.s_pfree = 0 + 3 * 1;
+    super_block.s_pfree = datas + 3 * blocksi;
 }
 
 unsigned int balloc() {
     /*
     从当前空闲块堆栈中返回一个当前空闲块id
+    返回值为空闲块地址
+    若返回值为-1,则说明虚拟盘中无空闲空间,分配失败
      */
-    unsigned free_block, free_block_num;
+
+    if (super_block.s_nfree == 0) {
+        return -1;
+    }
+
+    unsigned int free_block;
+
+    free_block = super_block.s_pfree;
+
+    if (super_block.s_free[0] != 1) {
+        super_block.s_free[0]--;
+        if (super_block.s_free[1] == 0)
+            super_block.s_pfree = super_block.s_free[super_block.s_free[0] + 1];
+        else
+            super_block.s_pfree = super_block.s_free[super_block.s_free[0]];
+        super_block.s_nfree--;
+    } else {
+        if (super_block.s_free[1] != 0) {
+            //如果当前不是最后一块
+            //读super_block.s_free[1]处的block并付给super_block.sfree[];
+            /// TODO 读super_block
+            super_block.s_pfree = super_block.s_free[super_block.s_free[0]];
+            super_block.s_nfree--;
+        } else {
+            //当前块为最后一块,即最后一块也用完了
+            super_block.s_pfree = 0;
+            super_block.s_nfree--; //自减后应为0
+        }
+    }
+    return free_block;
 }
 
 void bfree(unsigned int block_num) {
     /*
     释放一个空闲块,加入到当前空闲块堆栈中
      */
+    super_block.s_nfree++;
+
+    if ((super_block.s_pfree == NICFREE && super_block.s_free[1] != 0) ||
+        super_block.s_pfree == NICFREE - 1 && super_block.s_free[1] == 0) {
+        //此种状态下要将super.s_free[]写到虚拟盘中了
+        //要写到的地址为block_num
+        // TODO 写回磁盘
+
+        //更新当前空闲块堆栈
+        super_block.s_free[0] = 1;
+        super_block.s_free[1] = block_num;
+        super_block.s_pfree = super_block.s_free[0];
+    } else {
+        super_block.s_free[0]++;
+        super_block.s_free[super_block.s_free[0]] = block_num;
+        super_block.s_pfree = super_block.s_free[super_block.s_free[0]];
+    }
+
+    // TODO 将blcok_num地址的内容清空
 }
 
 int main() {
     format_sb_freedi();
-    return 0;
+    cout << endl;
+    for (int i = 0; i < 20; i++) {
+        cout << balloc() << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 1; i++) {
+        bfree(5);
+    }
+    cout << balloc() << " ";
+    cout << endl;
 }
--- ballocfree.cpp
+++ ballocfree.cpp
@@ -10,27 +10,22 @@ void format_sb_freedi() {
     int free_dblock = ALLBLOCKNUM - (2 + dinodeBLK);
     super_block.s_nfree = free_dblock;
 
-    //初始化空闲块
-    //从最后一块开始分组
     for (int i = free_dblock / NICFREE; i > 0; i--) {
         if (i == free_dblock / NICFREE) {
             // 最后一块
             super_block.s_free[0] = NICFREE - 1;
-            super_block.s_free[1] = -1;
+            super_block.s_free[1] = 0;
         } else {
             super_block.s_free[0] = NICFREE;
-            super_block.s_free[1] =
-                DATASTART +
-                (i * NICFREE - 1) * BLOCKSIZ; //指向下一个记录空闲块的物理块
+            super_block.s_free[1] = DATASTART + (i * NICFREE - 1) * BLOCKSIZ;
         }
         for (int j = 2; j < NICFREE + 1; j++) {
             super_block.s_free[j] = DATASTART + (i * NICFREE - j) * BLOCKSIZ;
         }
         if (i == 1) {
-            //第一个磁盘块
-            //写到第4个block
             super_block.s_free[0] = NICFREE - 3;
-            super_block.s_nfree -= 3; // 分配了三个,减3
+            super_block.s_nfree -= 3;
+            //写到 DATASTART + 3 *　NICFREE * BLOCKSIZ
             // TODO 把第一个空闲组 -> 写到第4个盘
 
         } else {
@@ -40,21 +35,71 @@ void format_sb_freedi() {
     }
     // TODO 处理多出来的磁盘
 
-    //因为前三个已经被占用,所以空闲块堆栈指针指向当前空的第一个数据块->即第4个
     super_block.s_pfree = DATASTART + 3 * BLOCKSIZ;
 }
 
 unsigned int balloc() {
     /*
     从当前空闲块堆栈中返回一个当前空闲块id
+    返回值为空闲块地址
+    若返回值为-1,则说明虚拟盘中无空闲空间,分配失败
      */
-    unsigned free_block, free_block_num;
+
+    if (super_block.s_nfree == 0) {
+        return -1;
+    }
+
+    unsigned int free_block;
+
+    free_block = super_block.s_pfree;
+
+    if (super_block.s_free[0] != 1) {
+        super_block.s_free[0]--;
+        if (super_block.s_free[1] == 0)
+            super_block.s_pfree = super_block.s_free[super_block.s_free[0] + 1];
+        else
+            super_block.s_pfree = super_block.s_free[super_block.s_free[0]];
+        super_block.s_nfree--;
+    } else {
+        if (super_block.s_free[1] != 0) {
+            //如果当前不是最后一块
+            //读super_block.s_free[1]处的block并付给super_block.sfree[];
+            /// TODO 读super_block
+            super_block.s_pfree = super_block.s_free[super_block.s_free[0]];
+            super_block.s_nfree--;
+        } else {
+            //当前块为最后一块,即最后一块也用完了
+            super_block.s_pfree = 0;
+            super_block.s_nfree--; //自减后应为0
+        }
+    }
+    return free_block;
 }
 
 void bfree(unsigned int block_num) {
     /*
     释放一个空闲块,加入到当前空闲块堆栈中
      */
+    super_block.s_nfree++;
+
+    if ((super_block.s_pfree == NICFREE && super_block.s_free[1] != 0) ||
+        super_block.s_pfree == NICFREE - 1 && super_block.s_free[1] == 0) {
+        //此种状态下要将super.s_free[]写到虚拟盘中了
+        //要写到的地址为block_num
+        //TODO 写回磁盘
+        
+        //更新当前空闲块堆栈
+        super_block.s_free[0] = 1;
+        super_block.s_free[1] = block_num;
+        super_block.s_pfree = super_block.s_free[0];
+    }else{
+        super_block.s_free[0]++;
+        super_block.s_free[super_block.s_free[0]] = block_num;
+        super_block.s_pfree = super_block.s_free[0];
+    }
+
+
+    //TODO 将blcok_num地址的内容清空
 }
 
 int main() { format_sb_freedi(); }

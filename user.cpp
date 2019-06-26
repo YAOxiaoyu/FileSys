73536ef4fc348cfd85514c930abcf65d0e482732
Rdeg <YuRdeg@163.com>
2019-06-26 13:46:51 +0800

    密码系统

--- user.cpp
+++ user.cpp
@@ -1,20 +1,66 @@
 #include "fileSys.h"
 #include<iostream>
+#include<string>
+#include<sstream>
 #include<conio.h>
-struct userListItem{
-	char uName[20];
-	unsigned short uId;
-	unsigned short gId;
-};
+using namespace std;
 
 enum mode{READ,WRITE,X};
-void login() {
+bool login() {
+	//u_default_mode
+	struct user userLogging;
+	cin >> userLogging.u_uid;
+	struct password pLogging;
+	pLogging.p_uid = userLogging.u_uid;
+	getPassword(pLogging.password);
+	int i;
+	for (i = 0; i < PWDNUM;i++) {//匹配密码
+		if (password[i].p_uid = pLogging.p_uid) {
+			for (int j = 0; j < PWDSIZ; j++) {
+				if (password[i].password[i] != pLogging.password[i]) {
+					cout << "password error!" << endl;
+					return false;
+				}
+			}
+			break;
+		}
+	}
+	if (i==PWDNUM) {
+		return false;
+	}
 
+	vector<struct user>::iterator findUser = userLogin.begin();
+	while (findUser!=userLogin.end()){
+		if (findUser->u_uid = userLogging.u_uid) {//已登录，切换用户
+			break;
+		}
+		findUser++;
+	}
+	if (findUser == userLogin.end()) {//未登录
+		userLogin.push_back(userLogging);
+	}
+	return true;
 }
 
-void logout() {
-
+void logout(struct user userQuit) {
+	//关闭全部文件
+	vector<struct user>::iterator findUser=userLogin.begin();
+	while (findUser != userLogin.end()) {
+		if (findUser->u_uid == userQuit.u_uid) {
+			userLogin.erase(findUser);
+			break;
+		}
+		findUser++;
+	}
+	if (findUser == userLogin.end()) {
+		cout << "error" << endl;
+		return;
+	}
+	get_dir("home");
+	login();
 }
+
+
 void getPassword(char *password) {
 	for (int i = 0; i < 20; i++) {
 		password[i] = '\0';
@@ -34,11 +80,11 @@ void getPassword(char *password) {
 	}
 }
 
-void createUser(char uName[20],unsigned short uId,unsigned short gId) {
+void createUser(unsigned short uId,unsigned short gId) {
 	userListItem userAdd;
-	for (int i = 0; i < 20; i++) {
+	/*for (int i = 0; i < 20; i++) {
 		userAdd.uName[i] = uName[i];
-	}
+	}*/
 	userAdd.uId = uId;
 	userAdd.gId = gId;
 	struct password pAdd;
@@ -46,9 +92,25 @@ void createUser(char uName[20],unsigned short uId,unsigned short gId) {
 	pAdd.P_gid = gId;
 	cout << "输入密码:";
 	getPassword(pAdd.password);//TODO:密码规则
-	//todo 写入用户列表，密码列表
+	if (pwdNum >= PWDNUM) {
+		cout << "用户过多" << endl;
+		return;
+	}
+	userList.push_back(userAdd);//写入用户列表，密码列表
+	password[pwdNum]=pAdd;
+	get_dir("home");
+	stringstream s;
+	string uIdS;
+	s << uId;
+	s >> uIdS;
+	mkdir(uIdS);
+	//TODO:回到原目录
 }
+
 bool access(inode* inode,struct user* user,mode m) {
+	if (user->u_uid = 0000) {
+		return true;
+	}
 	unsigned short acc= inode->di_mode;
 	unsigned short key=0b111111111;
 	if (user->u_uid == inode->di_uid) {
